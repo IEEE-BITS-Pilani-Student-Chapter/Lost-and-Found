f9b13561be00e13507fa4dfd049ae5073b7697d9
Prateek Kashyap <f20211449@pilani.bits-pilani.ac.in>
2022-06-17 21:05:34 +0530

    added google auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import SignIn from './Components/SignIn';
-import { Routes, Route } from "react-router-dom"
+import "./App.css";
+import SignIn from "./Components/SignIn";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/signin" element={<SignIn todo='SIGN IN'/>} />
-        <Route exact path="signup" element={<SignIn todo='SIGN UP'/>} />
+        <Route exact path="/" element={<SignIn todo="SIGN IN" />} />
+        <Route exact path="/signup" element={<SignIn todo="SIGN UP" />} />
       </Routes>
     </div>
   );
--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,38 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './SignIn.css'
+import GoogleAuth from "../GoogleAuth.js";
+import "./SignIn.css";
 
 function SignIn(props) {
-
-    return(
-        <div className='signIn'>
-            <div className="signIn-div">
-                <div className="select">
-                    <span className={props.todo === 'SIGN IN' ? "inactive-option" : ''}><Link className={props.todo === 'SIGN IN' ? "inactive-option-link" : ''} to="/signin">SIGN IN</Link></span>
-                    <span className={props.todo === 'SIGN UP' ? "inactive-option" : ''}><Link className={props.todo === 'SIGN UP' ? "inactive-option-link" : ''} to="/signup">SIGN UP</Link></span>
-                </div>
-
-                <form action="">
-                <div className="form-ele" style={{display: props.todo === 'SIGN UP' ? 'flex' : 'none'}}>
-                        <label htmlFor="">USERNAME</label>
-                        <input type="name" placeholder="Enter your username"/>
-                    </div>
-
-                    <div className="form-ele">
-                        <label htmlFor="">EMAIL</label>
-                        <input type="email" placeholder="Enter your email ID"/>
-                    </div>
-
-                    <div className="form-ele">
-                        <label htmlFor="">PASSWORD</label>
-                        <input type="text" name="" id="" placeholder="Enter your password"/>
-                    </div>
-                </form>
-
-                <div className="btn">{props.todo}</div>
-            </div>
+  return (
+    <div className="sign-in">
+      <div className="box">
+        <div className="select">
+          <span className={props.todo === "SIGN IN" ? "inactive-option" : ""}>
+            <Link
+              className={props.todo === "SIGN IN" ? "inactive-option-link" : ""}
+              to="/"
+            >
+              SIGN IN
+            </Link>
+          </span>
+          <span className={props.todo === "SIGN UP" ? "inactive-option" : ""}>
+            <Link
+              className={props.todo === "SIGN UP" ? "inactive-option-link" : ""}
+              to="/signup"
+            >
+              SIGN UP
+            </Link>
+          </span>
         </div>
-    )
+
+        <div className="form">
+          <div
+            className="form-ele"
+            style={{ display: props.todo === "SIGN UP" ? "flex" : "none" }}
+          >
+            <label htmlFor="">USERNAME</label>
+            <input type="name" placeholder="Enter your username" />
+          </div>
+
+          <div className="form-ele">
+            <label htmlFor="">EMAIL</label>
+            <input type="email" placeholder="Enter your email ID" />
+          </div>
+
+          <div className="form-ele">
+            <label htmlFor="">PASSWORD</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter your password"
+            />
+          </div>
+        </div>
+
+        <div className="btn">{props.todo}</div>
+        <GoogleAuth />
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
