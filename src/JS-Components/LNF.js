6c52ae328aa3adcd3819c8d60d93431ad5958117
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2022-07-13 23:23:34 +0530

    LNF form styling

--- src/JS-Components/LNF.js
+++ src/JS-Components/LNF.js
@@ -5,40 +5,60 @@ function LNF(props) {
   return (
     <div className="lnf" id="lnf">
       {/* form for both lost and found */}
+      <div className="lnf-form" id="lnf-form">
       <form action="">
         {/* item name */}
         <div className="lnf-form-div">
-          <label htmlFor="">Item's Name</label>
-          <input type="text" />
+          <input type="text" name="item_name" autoComplete="off" required />
+          <label htmlFor="item_name">Item's Name</label>
         </div>
 
         {/* location */}
         <div className="lnf-form-div">
           {/* input label acc to lost or found */}
-          <label htmlFor="">
-            {props.l_or_f === "l" ? "Last Seen At" : "Found At"}
-          </label>
-          <input type="text" name="" id="" />
+          <input type="text" name="place" id="" autoComplete="off" required />
+          <label htmlFor="place">{props.l_or_f === "l" ? "Where did you last see that?" : "Where did you found that?"}</label>
+        </div>
+
+        {/* upload img */}
+        <div className="lnf-form-div">
+          <span>Upload an image</span>
+          <input type="file" class="upload" placeholder="Upload Image" required />
         </div>
 
         {/* user name */}
         <div className="lnf-form-div">
-          <label htmlFor="">Your Name</label>
-          <input type="text" />
+          <input type="text" name="user_name" autoComplete="off" required />
+          <label htmlFor="user_name">Your Name</label>
         </div>
 
         {/* hostel */}
         <div className="lnf-form-div">
-          <label htmlFor="">Hostel and Room No</label>
-          <input type="text" />
+          <input type="text" name="hostel"  autoComplete="off" required/>
+          <label htmlFor="hostel">Hostel and Room No.</label>
         </div>
 
         {/* contact */}
         <div className="lnf-form-div">
-          <label htmlFor="">Contact No.</label>
-          <input type="number" />
+          <input type="number" name="contact" autoComplete="off" required pattern="[0-9]{10,10}" />
+          <label htmlFor="contact">Contact</label>
+        </div>
+
+        {/* submit */}
+        <div className="lnf-form-div lnf-form-submit">
+          <input type="submit" className="submit" />
         </div>
       </form>
+    </div>
+
+    <div className="lnf-text">
+      <div>
+      {props.l_or_f === 'l' ? 'LOST' : 'FOUND'}
+      </div>
+      <div>
+        SOMETHING?
+      </div>
+    </div>
     </div>
   );
 }
